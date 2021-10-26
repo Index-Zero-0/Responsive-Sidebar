aed95ebc72b8c20581ea3225f33398ca6ef6a7fa
Index-Zero <index.zero.00@gmail.com>
2021-10-26 19:51:31 +0200

    init

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const navButton = document.querySelector(".navbarButton");
+const navbar = document.querySelector("nav");
+
+navButton.addEventListener("click", () => {
+	navbar.classList.toggle("hide-navbar");
+})
